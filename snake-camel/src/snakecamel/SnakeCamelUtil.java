5eaacd43919ac550328b6b0313d7a914f0789a06
g12951te <g12951te@m31306.tsuda.ac.jp>
2015-04-29 15:38:45 +0900

    エラーを修正

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -5,12 +5,11 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
 	}
-	 
 	public static String camelToSnakecase(String camelcase) {
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
@@ -22,7 +21,7 @@ public class SnakeCamelUtil {
 					sb.append("_");
 				}
 				sb.append(Character.toLowerCase(c));
-				j = i;
+				j = i+1;
 			}
 		}
 		sb.append(camelcase.substring(j));
--- snake-camel/test/snakecamel/SnakeCamelUtilTest.java
+++ snake-camel/test/snakecamel/SnakeCamelUtilTest.java
@@ -1,5 +1,6 @@
 package snakecamel;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,8 +8,90 @@ import org.junit.Test;
 public class SnakeCamelUtilTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void capitalize‚Å“ª•¶š‚ğ‘å•¶š‚É•ÏŠ·‚Å‚«‚é() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "Capitalize";
+		String actual = scu.capitalize("capitalize");
+		assertThat(actual, is(expected));
 	}
 
+	@Test
+	public void capitalize‚Å“ª•¶š‚ª‘å•¶š‚É‚È‚Á‚Ä‚¢‚é•¶š—ñ‚ğ“ü‚ê‚½ê‡() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "Capitalize";
+		String actual = scu.capitalize("Capitalize");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void uncapitalize‚Å“ª•¶š‚ğ¬•¶š‚É•ÏŠ·‚Å‚«‚é() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "uncapitalize";
+		String actual = scu.uncapitalize("Uncapitalize");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void uncapitalize‚Å“ª•¶š‚ª¬•¶š‚É‚È‚Á‚Ä‚¢‚é•¶š—ñ‚ğ“ü‚ê‚½ê‡() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "uncapitalize";
+		String actual = scu.uncapitalize("uncapitalize");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void snake_camel‚ªSnakeCamel‚É•ÏŠ·‚Å‚«‚é() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "SnakeCamel";
+		String actual = scu.snakeToCamelcase("snake_camel");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void snake_camel‚ÉSnake_camel‚ğ‘ã“ü‚·‚é() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "SnakeCamel";
+		String actual = scu.snakeToCamelcase("Snake_camel");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void snake_camel‚ÉSnake_Camel‚ğ‘ã“ü‚·‚é() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "SnakeCamel";
+		String actual = scu.snakeToCamelcase("Snake_Camel");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void camelToSnakecase‚ÅSnakeCamel‚ğsnake_camel‚É•ÏŠ·‚Å‚«‚é() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "snake_camel";
+		String actual = scu.camelToSnakecase("SnakeCamel");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void camelToSnakecase‚ÉSnakecamel‚ğ‘ã“ü‚·‚é() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "snakecamel";
+		String actual = scu.camelToSnakecase("Snakecamel");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void camelToSnakecase‚ÉsnakeCamel‚ğ‘ã“ü‚·‚é() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "snake_camel";
+		String actual = scu.camelToSnakecase("snakeCamel");
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void camelToSnakecase‚Ésnakecamel‚ğ‘ã“ü() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "snakecamel";
+		String actual = scu.camelToSnakecase("snakecamel");
+		assertThat(actual, is(expected));
+	}
 }
